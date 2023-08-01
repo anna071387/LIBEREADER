4bffa656af8e72ecbb10ab103c36b3c2223ed804
Anna Gallagher <annagallagher713@gmail.com>
2023-07-31 21:15:32 -0600

    updated conterolllers folder with books and users, indexjs,added lodash npm package, added public js folder with login, logout, profile.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const booksRoutes = require('./booksRoutes');
+
 
 router.use('/users', userRoutes);
+router.use('/books', booksRoutes);
 
 module.exports = router;
