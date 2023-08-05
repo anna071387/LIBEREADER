922d1b1cd6ded46c3514e1fd476297467eebefb1
Bryce Debernarde <brycedebernarde@Bryces-MBP.lan>
2023-08-04 22:02:43 -0600

    merging code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const booksRoutes = require('./booksRoutes');
+const userRoutes = require('./UserRoutes');
+const booksRoutes = require('./BooksRoutes');
 
 
 router.use('/users', userRoutes);
--- models/books.js
+++ models/books.js
@@ -1,40 +1,34 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
+
 const sequelize = require('../config/connection');
 
-
-class Books extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
-}
+class Books extends Model {}
 
 Books.init(
-    {
-      genre: {
-        type: DataTypes.STRING, 
-        allowNull: false,
-
-      },  
-      author: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
+  {
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-      {
-        sequelize,
-        modelName: 'books',
-      }
-    );
-  
-    module.exports = Books;
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'books',
+  }
+);
+
+module.exports = Books;
